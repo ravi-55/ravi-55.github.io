ee70cfca4781f228756016b78ef0cd87f818eb51
Ravi Pardeshi <ravipardeshia52@gmail.com>
2021-07-23 23:32:37 +0530

    New Updates

--- animeQuotes/JS/index.js
+++ animeQuotes/JS/index.js
@@ -1,35 +1,56 @@
-let count = 0;
+const box = document.getElementById('box')
+const anime = document.getElementById('anime')
+const quoteText = document.getElementById('quote')
+const character = document.getElementById('author')
+const loader = document.getElementById('loader')
 
-// default Quote
-$('#quote').html(quotes[count].quote);
-$('#author').html(quotes[count].author);
+// Show Loading
+function loading(){
+	loader.hidden = false;
+	box.hidden = true;
+}
+
+// Hide Loading
+function complete(){
+	if(!loader.hidden){
+		box.hidden = false;
+		loader.hidden = true;
+	}
+}
+
+
+// Get Quote from API
+async function getQuotes(){
+	loading();
+	const apiURL = 'https://animechan.vercel.app/api/random';
+	try{
+		const response = await fetch(apiURL);
+		const data = await response.json();
+		anime.innerText = data.anime;
+		quoteText.innerText = data.quote;
+		character.innerText = data.character;
+		complete();
+	}
+	catch(error){
+		getQuotes();
+	}
+}
+getQuotes();
+
+// Copy Quote
+$('#copy').click(function(event){
+	event.preventDefault();
+	var $temp = $("<input>");
+	$("body").append($temp);
+	$temp.val($('#quote').text()).select();
+	document.execCommand("copy");
+	$temp.remove();
+	$(this).text('Copied')
+});
 
 // Next Quote
-$('#next').click(function(){
-	count ++;
-	load(count);
-	if (count > 0) 
-		disable('previous', false);
-	if (count == quotes.length-1)
-		disable('next', true)
+$('#next').click(function(event){
+	event.preventDefault();
+	getQuotes();
+	$('#copy').text('Copy')
 });
-
-// Previous Quote
-$('#previous').click(function(){
-	count --;
-	load(count)
-	if(count == 0)
-		disable('previous', true)
-	disable('next', false)
-});
-
-// Load Quotes
-function load(index){
-	$('#quote').html(quotes[index].quote);
-	$('#author').html(quotes[index].author);
-}
-
-// Disable or Anable the Buttons
-function disable(element, value){
-	$('#' + element).prop('disabled', value);
-}
